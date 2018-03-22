56c5e310b5c59841b7662b88161770ee90a3142c
wulechuan <wulechuan@live.com>
2018-03-22 16:51:26 +0800

    almost done

---
+++ ReadMe.md
@@ -0,0 +1,45 @@
+<link rel="stylesheet" href="./dist/markdown-styles-for-vscode-built-in-preview.min.css">
+
+# Introduction
+
+This is a collection of themes that each
+can be applied to shade any `Markdown` generated `HTML` contents.
+
+
+
+# Usage
+
+
+## Offline Usage
+
+For offline usage, like working with Microsft [Visual Studio Code](https://code.visualstudio.com),
+it's as easy as adding a markup inside your `Markdown` files, like so:
+
+```md
+<link rel="stylesheet" href="path/to/markdown-styles-for-vscode-built-in-preview.min.css">
+```
+
+
+## Online Usage
+
+Currently, there is **no** way
+that we can insert customer stylesheets into markdown pages in `github.com` or `bitbucket.org`.
+
+See <https://github.com/github/markup> for a reference.
+
+> 2. The HTML is sanitized, aggressively removing things
+>    that could harm you and your kin—such as
+>   `script` tags, `inline-styles`, and `class` or `id` attributes.
+>    See the sanitization filter for the full whitelist.
+
+
+
+
+
+# Example
+
+Below is a snapshot of the rendered look of an `.md` file,
+inside Microsoft Visual Studio Code,
+rendered by the built-in markdown preview tool.
+
+![Markdown Rendered Example 1](./docs/illustrates/markdown-theme-example.png "Markdown Rendered Example 1")
---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+const gulp = require('gulp');
+const { sync: deleteFilesSync } = require('del');
+
+const scopedGlobsLazilyWatchingMechanism = require('@wulechuan/scoped-glob-watchers');
+const gulp3CommonPipelines               = require('@wulechuan/gulp-3-common-pipeline-presets');
+
+const gulp3PipelineUtils = gulp3CommonPipelines.utilities;
+
+const {
+	printCompletionOfOneTask, // eslint-disable-line no-unused-vars
+	forSettingsOfScopedLazyWatchers,
+} = gulp3PipelineUtils;
+
+const buildACSSStylusBuildingPipeline = gulp3CommonPipelines.specificPipelines.css.stylusCompilation;
+
+const npmProjectRootPath = process.cwd();
+
+/*
+*
+*
+*
+*
+*
+*
+* *****************************************************
+* 利用【工作流预设（pipeline presets）】构建的整套整套的任务集
+* *****************************************************
+*/
+
+const allPipelinesForCSSOfDocs = [
+	// 'markdown-styles-for-embedding',
+	'markdown-styles-for-vscode-built-in-preview',
+].map((entryFileBaseName, index) => {
+	return buildACSSStylusBuildingPipeline({
+		taskNameKeyPart: `Markdown CSS (${index})`,
+		sourceBasePath: 'source',
+		outputBasePathOfBuilding: 'dist',
+		buildingEntryGlobsRelativeToSourceBasePath: [ `${entryFileBaseName}.styl` ],
+		builtSingleFileBaseName: entryFileBaseName,
+		shouldNotGenerateMinifiedVersions: false
+	});
+})
+
+/*
+*
+*
+*
+*
+*
+*
+* ****************************************
+*                Watchers
+* ****************************************
+*/
+
+const scopedWatchingSettings = {};
+
+forSettingsOfScopedLazyWatchers.appendMoreScopesViaPipelines({
+	scopedWatchingSettingsToModify: scopedWatchingSettings,
+	shouldTakeActionOnWatcherCreation: true,
+	involvedPipelines: [
+		...allPipelinesForCSSOfDocs,
+	],
+});
+
+/*
+*
+*
+*
+*
+*
+*
+* ****************************************
+*             Compound Tasks
+* ****************************************
+*/
+
+gulp.task('clean', [
+	...allPipelinesForCSSOfDocs,
+].map(pipeline => pipeline.taskNameOfDeletingFiles), thisTaskIsDone => {
+	deleteFilesSync(staticFilesDeploymentRootPath);
+	printCompletionOfOneTask('Delete "static" folder for deployment');
+	thisTaskIsDone();
+});
+
+gulp.task('build once', [
+	...allPipelinesForCSSOfDocs,
+].map(pipeline => pipeline.taskNameOfMainTask));
+
+gulp.task('build and then watch', (thisTaskIsDone) => {
+	scopedGlobsLazilyWatchingMechanism.createWatchersAccordingTo(scopedWatchingSettings, {
+		basePathForShorteningPathsInLog: npmProjectRootPath,
+	});
+	thisTaskIsDone();
+});
+
+
+gulp.task('default', ['build and then watch']);
