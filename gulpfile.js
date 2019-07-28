1d6eb0b23347d092d5bad9134529bfbb9e9ed13a
wulechuan <wulechuan@live.com>
2019-07-29 03:25:20 +0800

    struggling on gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,19 @@
 const path = require('path')
 
 const gulp = require('gulp')
-const gulpSequence = require('gulp-sequence')
-const gaze = require('gaze')
+const {
+	src: gulpRead,
+	dest: gulpWrite,
+	series: gulpBuildTaskSeries,
+	parallel: gulpBuildParallelTasks,
+	watch
+} = gulp
 
 
-const rename = require('gulp-rename')
 const del = require('del')
 
-
+const gulpIf = require('gulp-if');
+const rename = require('gulp-rename')
 const concatInto = require('gulp-concat')
 const minifyCSS = require('gulp-cssmin')
 
@@ -18,105 +23,180 @@ const cssminOptions = {
 }
 
 
+
+
+// Globs and task settings
+
 const sourceRootFolderPath = 'source'
 const outputRootFolderPath = 'dist'
 
 const subPathOfSourceCSS = 'hand-coded-new-version'
-
-const allGlobsToWatch = path.join(sourceRootFolderPath, '**/*')
-
-const cssTasksSettings = {
-	// 'Building CSS: generic version': {
-	// 	cssSourceGlobs: [ '*.css' ],
+const allCSSTasks = [
+	// {
+	// 	description: 'Building CSS: the generic version',
+	// 	sourceGlobsCommonSubPath: subPathOfSourceCSS,
+	// 	sourceRelativeGlobs: [ '*.css' ],
 	// 	outputFolderPath: outputRootFolderPath,
-	// 	outputCSSFileBaseName: 'wulechuan-style-for-html-via-markdwon',
+	// 	outputFileBaseName: 'wulechuan-style-for-html-via-markdwon',
 	// },
-	'Building CSS: firefox addon specific version': {
-		cssSourceGlobs: [ '*.css' ],
+	{
+		description: 'Building CSS: specifically for firefox addon "Markdown Viewer Webext"',
+		sourceGlobsCommonSubPath: subPathOfSourceCSS,
+		sourceRelativeGlobs: [ '*.css' ],
 		outputFolderPath: outputRootFolderPath,
-		outputCSSFileBaseName: 'wulechuan-style-for-html-via-markdwon--firefox-addon',
+		outputFileBaseName: 'wulechuan-style-for-html-via-markdwon--firefox-addon',
 	},
-}
+]
 
 
-
-
-
-
-function createGulpTasksForBuildingCSS(gulpTaskName, options) {
+function createGulpTaskBodiesViaSettings(taskSettings) {
 	const {
-		sourceCSSRelativeGlobs,
+		sourceGlobsCommonSubPath,
+		sourceRelativeGlobs,
 		outputFolderPath,
-		outputCSSFileBaseName,
-	} = options
+		outputFileBaseName,
+		outputFileExt,
+		shouldNotOutputUncompressedVersion,
+		shouldOutputCompressedVersion,
+		compressor,
+		compressorOptions,
+	} = taskSettings
 
-	const cssSourceGlobs = sourceCSSRelativeGlobs.map(relativeGlob => {
-		return path.join(sourceRootFolderPath, subPathOfSourceCSS, relativeGlob)
+	if (!sourceRelativeGlobs) {
+		throw TypeError('"sourceRelativeGlobs" is required for a task settings object.')
+	}
+
+	if (!outputFileBaseName) {
+		throw TypeError('"outputFileBaseName" is required for a task settings object.')
+	}
+
+	if (!outputFileExt) {
+		throw TypeError('"outputFileExt" is required for a task settings object.')
+	}
+
+
+
+
+
+	const cssSourceGlobs = sourceRelativeGlobs.map(relativeGlob => {
+		return path.join(sourceRootFolderPath, sourceGlobsCommonSubPath, relativeGlob)
 	})
 
-	const gulpPreparationTaskName = `${gulpTaskName} - preparations`
+	const outputFileName1 = `${outputFileBaseName}.${outputFileExt}`
+	const outputFileName2 = `${outputFileBaseName}.min.${outputFileExt}`
 
-	const outputCSSFileName1 = `${outputCSSFileBaseName}.css`
-	const outputCSSFileName2 = `${outputCSSFileBaseName}.min.css`
+	const outputFileNames = []
+	
+	if (!shouldNotOutputUncompressedVersion) {
+		outputFileNames.push(outputFileName1)
+	}
 
-	const outputFilePaths = [
-		outputCSSFileName1,
-		outputCSSFileName2,
-	].map(baseName => path.join(outputFolderPath, baseName))
+	if (shouldOutputCompressedVersion) {
+		outputFileNames.push(outputFileName2)
+	}
 
-	gulp.task(gulpPreparationTaskName, () => {
-		console.log('Deleting these files if exist:')
-		outputFilePaths.forEach(filePath => console.log('\t', filePath))
-		return del(outputFilePaths)
-	})
+	taskSettings.sourceGlobsToWatch = cssSourceGlobs
+	taskSettings.outputFileNames = outputFileNames
 
-	gulp.task(gulpTaskName, [ gulpPreparationTaskName ], () => {
-		return gulp.src(cssSourceGlobs)
-			.pipe(concatInto(outputCSSFileName1))
-			.pipe(gulp.dest(outputFolderPath))
+	if (!taskSettings.description) {
+		taskSettings.description = `producing "${outputFileName1}"`
+	}
 
-			.pipe(minifyCSS(cssminOptions))
-			.pipe(rename((fullPathName) => {
-				fullPathName.basename += '.min'
-				return fullPathName
-			}))
-			.pipe(gulp.dest(outputFolderPath))
-	})
+	if (!taskSettings.taskBodies) {
+		taskSettings.taskBodies = {}
+	}
+
+
+
+	const {
+		description: taskDescription,
+		taskBodies,
+	} = taskSettings
+
+	taskBodies.buildOnce = gulpBuildTaskSeries(taskPreparations, taskMainPart)
+
+
+
+
+	function taskPreparations() {
+		const possibleOutputFilePaths = [
+			outputFileName1,
+			outputFileName2,
+		].map(baseName => path.join(outputFolderPath, baseName))
+
+		console.log('[Task] Deleting these files if exist:')
+		possibleOutputFilePaths.forEach(filePath => console.log('\t', filePath))
+
+		return del(possibleOutputFilePaths)
+	}
+
+	function taskMainPart() {
+		console.log(`[Task] "${taskDescription}"`)
+
+		return gulpRead(cssSourceGlobs)
+			.pipe(concatInto(outputFileName1))
+
+
+			.pipe(gulpIf(
+				!shouldNotOutputUncompressedVersion,
+				gulpWrite(outputFolderPath)
+			))
+
+
+			.pipe(gulpIf(
+				shouldOutputCompressedVersion,
+				compressor(compressorOptions)
+			))
+			.pipe(gulpIf(
+				shouldOutputCompressedVersion,
+				rename((fullPathName) => {
+					fullPathName.basename += '.min'
+					return fullPathName
+				})
+			))
+			.pipe(gulpIf(
+				shouldOutputCompressedVersion,
+				gulpWrite(outputFolderPath)
+			))
+	}
 }
 
-Object.keys(cssTasksSettings).forEach(gulpTaskName => {
-	createGulpTasksForBuildingCSS( gulpTaskName, cssTasksSettings[gulpTaskName])
-})
+
+function createGulpTaskBodiesForBuildingCSS(taskSettings) {
+	taskSettings.outputFileExt = 'css'
+
+	// taskSettings.shouldNotOutputUncompressedVersion = false
+	taskSettings.shouldOutputCompressedVersion = true
+
+	taskSettings.compressor = minifyCSS
+	taskSettings.compressorOptions = cssminOptions
+
+	createGulpTaskBodiesViaSettings(taskSettings)
+}
+
+
+
+allCSSTasks.forEach(createGulpTaskBodiesForBuildingCSS)
 
 
 
 
-
-
-// Top level tasks
-gulp.task('build-once', Object.keys(cssTasksSettings))
-gulp.task('build-and-watch', [ 'build-once' ], (onThisTaskDone) => {
-	gaze(allGlobsToWatch, (error, watcher) => {
-		if (error) {
-			throw error
-		}
-
-		const watchedFiles = watcher.relative()
-		console.log('watching:\n', watchedFiles)
-
-		watcher.on('all', (event, filepath) => {
-			gulpSequence('build-once')(onThisTaskDone)
-		})
-	})
-})
-gulp.task('clean', () => {
+// Public tasks
+exports.clean = function () {
 	console.log(`Deleting all built files in "${pathBuildRoot}"...`)
 	del(path.join(pathBuildRoot, '**/*'))
-})
+}
 
+exports.buildOnce = gulpBuildParallelTasks(
+	allCSSTasks.map(taskSettings => taskSettings.taskBodies.buildOnce)
+)
 
-
+exports.buildAndWatch = gulpBuildParallelTasks(
+	allCSSTasks.map(taskSettings => {
+		return watch(taskSettings.sourceGlobsToWatch, taskSettings.taskBodies.buildOnce)
+	})
+)
 
 
 // The default task
-gulp.task('default', [ 'build-once' ])
+exports.default = exports.buildAndWatch
