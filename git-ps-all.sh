aeb4935d073a1976bbb1f21f92afee774c9d0ded
吴乐川 <wulechuan@live.com>
2020-02-11 01:40:57 +0800

    添加了方便git推送的的sh

---
+++ git-ps-all.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+echo
+echo 开始
+echo
+
+echo
+echo --- 阿里云 --------------------------
+echo
+
+git ps aliyun
+
+echo
+echo
+echo
+echo --- 码云 ----------------------------
+echo
+
+git ps gitee
+
+echo
+echo
+echo
+echo --- GitHub -------------------------
+echo
+
+git ps github
+
+echo
+echo 结束
+echo
