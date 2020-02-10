b8ac2b8f87ff26e13a53ab88dee11a0d4e32e946
吴乐川 <wulechuan@live.com>
2020-02-11 01:33:22 +0800

    eslint 规则调整。奇怪，从前怎么通过lint的呢？

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
         es6: true,
         node: true,
     },
-    extends: 'eslint:recommended',
+    extends: [
+        'eslint:recommended',
+    ],
     parserOptions: {
         ecmaVersion: 2018,
         ecmaFeatures: {
@@ -12,37 +14,48 @@ module.exports = {
         sourceType: 'module',
     },
     rules: {
-        indent: [ 'error', 4, {
+        'no-unused-vars': 0, // for typescript interfaces
+        'import/no-unresolved': 0,
+        'import/no-extraneous-dependencies': 0,
+        'no-console':  [ 0 ],
+        'no-debugger': [ 2 ],
+        'no-multi-spaces': 0,
+        'no-implicit-globals': 0,
+        indent: ['error', 4, {
             SwitchCase: 1,
             MemberExpression: 1,
         }],
-        'no-trailing-spaces': [ 'error', {
+        'no-trailing-spaces': ['error', {
             skipBlankLines: false,
             ignoreComments: false,
         }],
-        'linebreak-style':[ 0, 'unix' ],
+        'linebreak-style': [0, 'unix'],
         'no-param-reassign': 0,
-        quotes: [ 'error', 'single' ],
-        semi: [ 'error', 'never' ],
-        'max-statements': [ 0, 32 ],
-        'multiline-ternary': [ 'error', 'always-multiline' ],
-        'no-console': [ 0 ],
-        'new-parens': [ 0 ],
-        'yoda': [ 'error', 'never' ],
-        'comma-dangle': [ 'error', 'always-multiline' ],
-        'func-names': [ 0, 'as-needed'],
-        'no-unused-vars': 'error',
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+        'max-statements': [0, 64],
+        'multiline-ternary': ['error', 'always-multiline'],
+        'new-parens': [1],
+        yoda: [0, 'never'],
+        'comma-dangle': ['error', {
+            arrays: 'always-multiline',
+            objects: 'always-multiline',
+            imports: 'always-multiline',
+            exports: 'always-multiline',
+            functions: 'never',
+        }],
+        'func-names': [0, 'as-needed'],
         'no-return-assign': 'error',
         'no-new-func': 'error',
         'no-eval': 'error',
         'no-implied-eval': 'error',
         'prefer-const': 'error',
         'arrow-body-style': 0,
-        'prefer-destructuring': [ 'error',
+        'prefer-destructuring': ['error',
             {
                 VariableDeclarator: {
                     array: true,
-                    'object': true,
+                    object: true,
                 },
                 AssignmentExpression: {
                     array: false,
@@ -53,7 +66,5 @@ module.exports = {
                 enforceForRenamedProperties: false,
             },
         ],
-        'import/no-unresolved': 0,
-        'import/no-extraneous-dependencies': 0,
     },
 }
