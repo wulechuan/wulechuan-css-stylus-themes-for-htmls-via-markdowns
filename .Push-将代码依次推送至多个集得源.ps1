dd894ebfa08f9fcfc1f6ce88de1b2b5c0e2f7686
吴乐川 <wulechuan@live.com>
2022-05-12 02:13:53 +0800

    更新了研发依赖包。不必升版本号。

--- .Push-将代码依次推送至多个集得源.ps1
+++ .Push-将代码依次推送至多个集得源.ps1
@@ -2,14 +2,10 @@
 
 .  '.\node_modules\@wulechuan\cli-scripts--git-push\源代码\PowerShell\Push-集得上推至单一源.ps1'
 
-Write-Host
-Write-Host  -F 'Black'  -B 'DarkGreen'  ' 【集得】上推（ git push ）至所有源：开始 '
-Write-Host
+Write-集得上推至一个或多个源_开始之提示语
 
-Push-集得上推至单一源  '吴乐川：阿里云'  -集得源之显示名称文本采用的颜色 'Green'
 Push-集得上推至单一源  '吴乐川：码云'    -集得源之显示名称文本采用的颜色 'Red'
+Push-集得上推至单一源  '吴乐川：阿里云'  -集得源之显示名称文本采用的颜色 'Blue'
 Push-集得上推至单一源  '吴乐川：GitHub'  -集得源之显示名称文本采用的颜色 'Yellow'
 
-Write-Host
-Write-Host  -F 'Black'  -B 'DarkGreen'  ' 【集得】上推（ git push ）至所有源：结束 '
-Write-Host
+Write-集得上推至一个或多个源_结束之提示语
