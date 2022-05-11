41465e82d507eaf2a99b408fc5d32c100d2fab1c
吴乐川 <wulechuan@live.com>
2022-05-12 02:17:43 +0800

    更新了研发依赖包。不必升版本号。

--- push-将代码依次推送至多个集得源.sh
+++ push-将代码依次推送至多个集得源.sh
@@ -2,9 +2,7 @@
 
 .  './node_modules/@wulechuan/cli-scripts--git-push/源代码/bash/吴乐川-集得上推至单一源.sh'
 
-echo
-echo -e "\e[42;30m 【集得】上推（ git push ）至所有源：开始 \e[0;0m"
-# echo
+吴乐川-集得上推至一个或多个源-开始之提示语
 
 吴乐川-集得上推至单一源  --should-skip=false \
     --git-origin-name='吴乐川：码云' \
@@ -14,13 +12,11 @@ echo -e "\e[42;30m 【集得】上推（ git push ）至所有源：开始 \e[0;
 吴乐川-集得上推至单一源  --should-skip=false \
     --git-origin-name='吴乐川：阿里云' \
     --git-origin-display-name='吴乐川：阿里云' \
-    --git-origin-display-name-color='green'
+    --git-origin-display-name-color='blue'
 
 吴乐川-集得上推至单一源  --should-skip=false \
     --git-origin-name='吴乐川：GitHub' \
     --git-origin-display-name='吴乐川：GitHub' \
     --git-origin-display-name-color='yellow'
 
-echo
-echo -e "\e[42;30m 【集得】上推（ git push ）至所有源：结束 \e[0;0m"
-echo
+吴乐川-集得上推至一个或多个源-结束之提示语
