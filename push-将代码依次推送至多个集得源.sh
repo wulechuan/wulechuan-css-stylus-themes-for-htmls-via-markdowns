0e62d15e3c7a626ca38a34eb2cb36b34b7210604
吴乐川 <wulechuan@live.com>
2022-05-12 17:39:35 +0800

    更新了命令行工具集。

--- push-将代码依次推送至多个集得源.sh
+++ push-将代码依次推送至多个集得源.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-.  './node_modules/@wulechuan/cli-scripts--git-push/源代码/bash/吴乐川-集得上推至单一源.sh'
+.  './node_modules/@wulechuan/cli-scripts--git-push/源代码/bash/吴乐川-集得源管理工具集.sh'
 
-吴乐川-集得上推至一个或多个源-开始之提示语
+吴乐川-集得上推至一个或多个源-显示开始之提示语
 
-吴乐川-集得上推至单一源  --should-skip=false \
+吴乐川-集得上推至单个源  --should-skip=false \
     --git-origin-name='吴乐川：码云' \
     --git-origin-display-name='吴乐川：码云' \
     --git-origin-display-name-color='red'
 
-吴乐川-集得上推至单一源  --should-skip=false \
+吴乐川-集得上推至单个源  --should-skip=false \
     --git-origin-name='吴乐川：阿里云' \
     --git-origin-display-name='吴乐川：阿里云' \
     --git-origin-display-name-color='blue'
 
-吴乐川-集得上推至单一源  --should-skip=false \
+吴乐川-集得上推至单个源  --should-skip=false \
     --git-origin-name='吴乐川：GitHub' \
     --git-origin-display-name='吴乐川：GitHub' \
     --git-origin-display-name-color='yellow'
 
-吴乐川-集得上推至一个或多个源-结束之提示语
+吴乐川-集得上推至一个或多个源-显示结束之提示语
--- 用于研发阶段的命令行工具/PowerShell/Add-添加所有集得源.ps1
+++ 用于研发阶段的命令行工具/PowerShell/Add-添加所有集得源.ps1
@@ -1,26 +1,17 @@
 #
 
-Write-Host
-Write-Host -F 'DarkRed' '===== 添加【集得】源： 吴乐川：码云  ========================='
-Write-Host
-git  remote  add  '吴乐川：码云'  'git@gitee.com:nanchang-wulechuan/wulechuan-css-stylus-themes-for-htmls-via-markdowns.git'
-Write-Host
-Write-Host
+$吴乐川的模块的路径 = '.\node_modules\@wulechuan\cli-scripts--git-push\源代码\PowerShell'
+Import-Module  "${吴乐川的模块的路径}\吴乐川-文本处理工具.psm1"
+Import-Module  "${吴乐川的模块的路径}\吴乐川-文本显示工具.psm1"
+Import-Module  "${吴乐川的模块的路径}\吴乐川-集得源管理工具集.psm1"
 
 
 
-Write-Host
-Write-Host -F 'DarkGreen' '===== 添加【集得】源： 吴乐川：阿里云  ========================='
-Write-Host
-git  remote  add  '吴乐川：阿里云'  'git@code.aliyun.com:wulechuan/wulechuan-themes-for-htmls-via-markdowns.git'
-Write-Host
-Write-Host
+Add-吴乐川添加单个集得源  -集得源在本机采用的名称 '吴乐川：码云'  -特征颜色 'DarkRed' `
+    -集得源之完整地址 'git@gitee.com:nanchang-wulechuan/wulechuan-css-stylus-themes-for-htmls-via-markdowns.git'
 
+Add-吴乐川添加单个集得源  -集得源在本机采用的名称 ' 吴乐川：阿里云'  -特征颜色 'Blue' `
+    -集得源之完整地址 'git@code.aliyun.com:wulechuan/wulechuan-themes-for-htmls-via-markdowns.git'
 
-
-Write-Host
-Write-Host -F 'Yellow' '===== 添加【集得】源： 吴乐川：GitHub  ========================='
-Write-Host
-git  remote  add  '吴乐川：GitHub'  'git@github.com:wulechuan/wulechuan-css-stylus-themes-for-htmls-via-markdowns.git'
-Write-Host
-Write-Host
+Add-吴乐川添加单个集得源  -集得源在本机采用的名称 '吴乐川：GitHub'  -特征颜色 'Yellow' `
+    -集得源之完整地址 'git@github.com:wulechuan/wulechuan-css-stylus-themes-for-htmls-via-markdowns.git'
